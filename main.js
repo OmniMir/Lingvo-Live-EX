97ffa88e9680517e23dead0281d0544016b4f5e4
kapsilon <kapsilon47@gmail.com>
2016-01-15 22:59:54 +0300

    Realease 0.1.1

--- main.js
+++ main.js
@@ -4,8 +4,7 @@ var buttonMinimize = document.getElementById('buttonMinimize');
 var buttonMaximize = document.getElementById('buttonMaximize');
 var buttonClose = document.getElementById('buttonClose');
 
-var insert_style = '.window-overlay::-webkit-scrollbar{height:33px;width:33px}.window-overlay::-webkit-scrollbar-thumb{min-height:50px;background:rgba(255,255,255,1);border-radius:17px;border:10px solid transparent;background-clip:padding-box}.window-overlay::-webkit-scrollbar-track-piece{background:rgba(0,0,0,.5);border:10px solid transparent;background-clip:padding-box}.window-overlay::-webkit-scrollbar-track-piece:vertical:start{border-radius:17px 17px 0 0}.window-overlay::-webkit-scrollbar-track-piece:vertical:end{border-radius:0 0 17px 17px}';
-var window_style = 'div.header-user{right: 96px;}';
+var insert_style = '';
 
 function windowMinimize() {
   chrome.app.window.current().minimize();
@@ -21,29 +20,6 @@ function windowMaximize() {
 function windowClose() {
     chrome.app.window.current().close();
 }
-
-// hide bullet if window is framed
-chrome.storage.sync.get(function(items) {
-    if (items.showFrame !== undefined) {
-        frame = items.showFrame;
-    }
-    if (frame == 'chrome') {
-        bullet.style.display = 'none';
-    } else {
-        insert_style += '.header-btn.header-boards,.promo-nav{margin-left:38px!important}';
-    }
-});
-
-// set some css on trello.com
-webview.addEventListener('loadcommit', function(e) {
-    if (e.isTopLevel) {
-        webview.insertCSS({
-            code: insert_style,
-            code: window_style,
-            runAt: 'document_start'
-        });
-    }
-});
 
 // send new-window-links to browser
 webview.addEventListener('newwindow', function(e) {
@@ -68,6 +44,7 @@ window.addEventListener('focus', function(e) {
     webview.focus();
 });
 
+//windows buttons actions
 buttonMinimize.addEventListener('click', windowMinimize);
 buttonMaximize.addEventListener('click', windowMaximize);
 buttonClose.addEventListener('click', windowClose);
